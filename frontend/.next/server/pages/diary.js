677ecd52068970ca7dcabf2e400de090b141d9d0
SuminSon <ssm0224@gmail.com>
2023-03-10 17:37:27 +0900

    [FE] new branch : 개발 환경 세팅

---
+++ frontend/.next/server/pages/diary.js
@@ -0,0 +1,66 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/diary";
+exports.ids = ["pages/diary"];
+exports.modules = {
+
+/***/ "./src/components/Seo.tsx":
+/*!********************************!*\
+  !*** ./src/components/Seo.tsx ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/head */ \"next/head\");\n/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_1__);\n\n\nconst Seo = (props)=>{\n    const { title  } = props;\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)((next_head__WEBPACK_IMPORTED_MODULE_1___default()), {\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"title\", {\n            children: [\n                \" \",\n                title,\n                \" | MUCHU \"\n            ]\n        }, void 0, true, {\n            fileName: \"C:\\\\Users\\\\SSAFY\\\\Desktop\\\\E203\\\\frontend\\\\src\\\\components\\\\Seo.tsx\",\n            lineNumber: 12,\n            columnNumber: 7\n        }, undefined)\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\SSAFY\\\\Desktop\\\\E203\\\\frontend\\\\src\\\\components\\\\Seo.tsx\",\n        lineNumber: 11,\n        columnNumber: 5\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Seo);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvY29tcG9uZW50cy9TZW8udHN4LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUM2QjtBQU03QixNQUFNQyxNQUFvQixDQUFDQyxRQUFVO0lBQ25DLE1BQU0sRUFBRUMsTUFBSyxFQUFFLEdBQUdEO0lBQ2xCLHFCQUNFLDhEQUFDRixrREFBSUE7a0JBQ0gsNEVBQUNHOztnQkFBTTtnQkFBRUE7Z0JBQU07Ozs7Ozs7Ozs7OztBQUdyQjtBQUNBLGlFQUFlRixHQUFHQSxFQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZnJvbnRlbmQvLi9zcmMvY29tcG9uZW50cy9TZW8udHN4PzljMmYiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHR5cGUgeyBGQyB9IGZyb20gXCJyZWFjdFwiO1xyXG5pbXBvcnQgSGVhZCBmcm9tIFwibmV4dC9oZWFkXCI7XHJcblxyXG5leHBvcnQgdHlwZSBTZW9Qcm9wcyA9IHtcclxuICB0aXRsZTogc3RyaW5nO1xyXG59O1xyXG5cclxuY29uc3QgU2VvOiBGQzxTZW9Qcm9wcz4gPSAocHJvcHMpID0+IHtcclxuICBjb25zdCB7IHRpdGxlIH0gPSBwcm9wcztcclxuICByZXR1cm4gKFxyXG4gICAgPEhlYWQ+XHJcbiAgICAgIDx0aXRsZT4ge3RpdGxlfSB8IE1VQ0hVIDwvdGl0bGU+XHJcbiAgICA8L0hlYWQ+XHJcbiAgKTtcclxufTtcclxuZXhwb3J0IGRlZmF1bHQgU2VvO1xyXG4iXSwibmFtZXMiOlsiSGVhZCIsIlNlbyIsInByb3BzIiwidGl0bGUiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/components/Seo.tsx\n");
+
+/***/ }),
+
+/***/ "./src/pages/diary/index.tsx":
+/*!***********************************!*\
+  !*** ./src/pages/diary/index.tsx ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Diary)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _components_Seo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @/components/Seo */ \"./src/components/Seo.tsx\");\n\n\nfunction Diary() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_components_Seo__WEBPACK_IMPORTED_MODULE_1__[\"default\"], {\n                title: \"Diary\"\n            }, void 0, false, {\n                fileName: \"C:\\\\Users\\\\SSAFY\\\\Desktop\\\\E203\\\\frontend\\\\src\\\\pages\\\\diary\\\\index.tsx\",\n                lineNumber: 6,\n                columnNumber: 7\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \" Diary\"\n            }, void 0, false, {\n                fileName: \"C:\\\\Users\\\\SSAFY\\\\Desktop\\\\E203\\\\frontend\\\\src\\\\pages\\\\diary\\\\index.tsx\",\n                lineNumber: 7,\n                columnNumber: 7\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                className: \"text-3xl font-bold underline bg-red-500\",\n                children: \"Hello world!\"\n            }, void 0, false, {\n                fileName: \"C:\\\\Users\\\\SSAFY\\\\Desktop\\\\E203\\\\frontend\\\\src\\\\pages\\\\diary\\\\index.tsx\",\n                lineNumber: 8,\n                columnNumber: 7\n            }, this)\n        ]\n    }, void 0, true);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvZGlhcnkvaW5kZXgudHN4LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQWtDO0FBRW5CLFNBQVNDLFFBQVE7SUFDOUIscUJBQ0U7OzBCQUNFLDhEQUFDRCx1REFBR0E7Z0JBQUNFLE9BQU07Ozs7OzswQkFDWCw4REFBQ0M7MEJBQUc7Ozs7OzswQkFDSiw4REFBQ0E7Z0JBQUdDLFdBQVU7MEJBQTBDOzs7Ozs7OztBQUc5RCxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZnJvbnRlbmQvLi9zcmMvcGFnZXMvZGlhcnkvaW5kZXgudHN4PzQzNzgiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFNlbyBmcm9tICdAL2NvbXBvbmVudHMvU2VvJ1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gRGlhcnkoKSB7XHJcbiAgcmV0dXJuIChcclxuICAgIDw+XHJcbiAgICAgIDxTZW8gdGl0bGU9XCJEaWFyeVwiIC8+XHJcbiAgICAgIDxoMT4gRGlhcnk8L2gxPlxyXG4gICAgICA8aDEgY2xhc3NOYW1lPVwidGV4dC0zeGwgZm9udC1ib2xkIHVuZGVybGluZSBiZy1yZWQtNTAwXCI+SGVsbG8gd29ybGQhPC9oMT5cclxuICAgIDwvPlxyXG4gIClcclxufVxyXG4iXSwibmFtZXMiOlsiU2VvIiwiRGlhcnkiLCJ0aXRsZSIsImgxIiwiY2xhc3NOYW1lIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/pages/diary/index.tsx\n");
+
+/***/ }),
+
+/***/ "next/head":
+/*!****************************!*\
+  !*** external "next/head" ***!
+  \****************************/
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./src/pages/diary/index.tsx"));
+module.exports = __webpack_exports__;
+
+})();

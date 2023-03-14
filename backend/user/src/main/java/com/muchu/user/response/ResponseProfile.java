9f2c0a96765839e8b8dfb35066c655dce64f1c87
jsieon97 <rudtn0820@naver.com>
2023-03-14 17:52:24 +0900

    API gateway, User회원가입, JWT로그인 생성

--- backend/user/src/main/java/com/muchu/user/response/ResponseProfile.java
+++ backend/user/src/main/java/com/muchu/user/response/ResponseProfile.java
@@ -1,5 +1,9 @@
 package com.muchu.user.response;
 
 public class ResponseProfile {
-    
+    private String userId;
+    private String profileImgPath;
+    private String introduce;
+    private Integer point;
+
 }

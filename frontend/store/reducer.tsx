24842bef704c52a51daf79e6d9ad00e540507537
mintcoo <xhaktm3344@gmail.com>
2023-03-15 17:35:37 +0900

    [FE]feature/setting: NextJS 세팅

---
+++ frontend/store/reducer.tsx
@@ -0,0 +1,22 @@
+import {combineReducers} from '@reduxjs/toolkit';
+import {HYDRATE} from 'next-redux-wrapper';
+import countSlice from './features/countSlice';
+
+// 리듀서들을 합쳐주는곳
+const combinedReducer = combineReducers({
+  counter: countSlice
+});
+
+// Hydrate와 위에서 합친 reducer들을 rootReducer에 세팅
+const rootReducer: typeof combinedReducer = (state, action) => {
+  // 서버 사이드 데이터를 클라이언트 사이드 Store에 통합.
+  if (action.type === HYDRATE) {
+    return {
+      ...state,
+      ...action.payload
+    };
+  }
+  return combinedReducer(state, action);
+};
+
+export default rootReducer;

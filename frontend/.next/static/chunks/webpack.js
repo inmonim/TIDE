a32101721fb04d7742f156157e7bc01ecbb58899
한상현 <xhaktm3344@gmail.com>
2023-03-17 18:00:36 +0900

    [FE]feature: 로그인 컴포넌트 제작중

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7bc46562534b6fa1"; }
+/******/ 		__webpack_require__.h = function() { return "53fa682a75b59001"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '@/styles/globals.css';
 import type {AppProps} from 'next/app';
 import {wrapper} from 'store';
 import MusicBar from '@/components/MusicBar';
+import { useState } from 'react';
 
 function App({Component, pageProps}: AppProps) {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
   return (
     <>
-      <MusicBar />
+      {isLogin && <MusicBar />}
       <Component {...pageProps} />
     </>
   );

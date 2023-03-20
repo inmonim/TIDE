655f807a0844ba2e6712194eab1b7de8dc272e10
한상현 <xhaktm3344@gmail.com>
2023-03-20 11:30:21 +0900

    [FE]login : 로그인 및 회원가입 기본css 페이지 이동 애니메이션 적용

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -24,13 +24,13 @@ const signup = () => {
 
   return (
     <div
-      className={`w-full h-screen flex justify-center text-white bg-[url('/images/BackGround/loginBG.png')] bg-no-repeat bg-cover sm:bg-[url('/images/BackGround/signupBG.png')] sm:bg-no-repeat sm:bg-cover sm:justify-end sm:min-h-[50rem]`}>
+      className={`w-full h-screen flex justify-center text-white bg-[url('/images/BackGround/loginBG.png')] bg-no-repeat bg-cover sm:bg-[url('/images/BackGround/signupBlackBG.png')] sm:bg-no-repeat sm:bg-cover sm:justify-end sm:min-h-[50rem]`}>
       <div
         className={`w-full mx-5 h-[95%] flex flex-col justify-center items-center bg-black bg-opacity-50 sm:w-[34rem] sm:h-full sm:bg-opacity-95 sm:mx-0`}>
         <div className={`w-40`}>
           <Image src={logoUrl} alt="logo" />
         </div>
-        <form className="flex flex-col items-center w-5/6 text-sm h-72 justify-evenly">
+        <form className="flex flex-col items-center w-5/6 text-sm h-[34rem] justify-evenly">
           <input
             type="email"
             className={`border-2 w-full h-10 rounded-md bg-transparent p-2 `}
@@ -42,24 +42,71 @@ const signup = () => {
             placeholder="패스워드"
           />
           <input
+            type="password"
+            className={`border-2 w-full h-10 rounded-md bg-transparent p-2`}
+            placeholder="패스워드 확인"
+          />
+          <div className="w-full text-lg text-left">생년월일</div>
+          <div className={`w-full`}>
+            <input
+              type="text"
+              className={`border-2 w-1/5 h-10 rounded-md bg-transparent p-2 mr-2`}
+              placeholder="년(4자리)"
+            />
+            <input
+              type="text"
+              className={`border-2 w-1/5 h-10 rounded-md bg-transparent p-2 mr-2`}
+              placeholder="월"
+            />
+            <input
+              type="text"
+              className={`border-2 w-1/5 h-10 rounded-md bg-transparent p-2 mr-2`}
+              placeholder="일"
+            />
+          </div>
+          <div className="w-full text-lg text-left">성별</div>
+          <div className={`w-full flex`}>
+            <input
+              type="radio"
+              name="gender"
+              className={`border-2 w-1/12 h-6`}
+              value="남자"
+              checked
+            />
+            <span>남자</span>
+            <input
+              type="radio"
+              name="gender"
+              className={`border-2 w-1/12 h-6`}
+              value="여자"
+            />
+            <span>여자</span>
+          </div>
+          <div className="w-full text-lg text-left">닉네임</div>
+          <div className={`w-full flex`}>
+            <input
+              type="text"
+              className={`border-2 w-full h-10 rounded-md bg-transparent p-2`}
+              placeholder="닉네임을 입력해주세요"
+            />
+          </div>
+
+          <input
             type="submit"
             className={`border-2 w-4/5 h-10 rounded-md bg-sky-700 text-sm cursor-pointer hover:bg-sky-500`}
-            value="로그인"
+            value="회원가입"
           />
         </form>
         <div className="w-1/2 border-[0.1rem] my-4"></div>
-        <div className={`w-60 flex flex-col h-60 justify-evenly items-center`}>
-          <div className={`w-full flex justify-center cursor-pointer`}>
-            <img src={google.current} alt="google" className="object-contain" />
-          </div>
-          <div className="text-md"> 계정이 없으신가요? </div>
+        <div className={`w-60 flex flex-col h-32 justify-evenly items-center`}>
+          <div className="text-md"> 계정이 이미 있으신가요? </div>
           <Link
             className={`border-2 w-full rounded-md bg-sky-700 hover:bg-sky-500`}
-            href={`/signup`}>
+            href={`/login`}>
             <input
               type="submit"
               className={`w-full h-10 cursor-pointer text-sm`}
-              value="회원가입"
+              value="로그인"
             />
           </Link>
         </div>

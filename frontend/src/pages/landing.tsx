90a294459167af19eea6ace68cd063f92438ad96
한상현 <xhaktm3344@gmail.com>
2023-03-21 15:55:51 +0900

    [FE] feature/youtube : 랜딩페이지 0.6초 딜레이

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -11,23 +11,20 @@ function LandingPage() {
   const firstDiv = useRef<HTMLDivElement>(null);
   const secondDiv = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    window.addEventListener('wheel', onWheelScroll);
-    return () => {
-      window.removeEventListener('wheel', onWheelScroll);
-    };
-  }, []);
-
   // 마우스 윌 이벤트 발생시
   const onWheelScroll = (event: any) => {
     const {deltaY} = event;
-    console.log(deltaY);
     if (deltaY > 0) {
-      secondDiv.current?.scrollIntoView({behavior: 'smooth'});
-    } else {
-      firstDiv.current?.scrollIntoView({behavior: 'smooth'});
-    }
+      setTimeout(() => {
+        secondDiv.current?.scrollIntoView({behavior: 'smooth'});
+      }, 600);
+    } else if (deltaY < 0) {
+      setTimeout(() => {
+        firstDiv.current?.scrollIntoView({behavior: 'smooth'});
+      }, 600);
+    } 
   };
+
 
   return (
     <>
@@ -37,10 +34,12 @@ function LandingPage() {
       </Head>
 
       {/* 전체 컨테이너 */}
-      <div className="flex flex-col w-full h-full justify-items-center">
+      <div
+        onWheel={onWheelScroll}
+        className="flex flex-col w-full h-full justify-items-center">
         {/* container1 */}
         <div
-          className="flex flex-row items-center content-center justify-center w-full pt-24 bg-[#021b30]"
+          className="flex flex-row items-center content-center justify-center w-full h-screen pt-24 bg-[#021b30]"
           ref={firstDiv}>
           {/* landingImage */}
           <div className="w-[1100px] h-[850px]">
@@ -81,7 +80,7 @@ function LandingPage() {
 
         <div
           ref={secondDiv}
-          className="flex flex-row items-center content-center justify-center w-full bg-[#021b30]">
+          className="flex flex-row items-center content-center justify-center w-full h-screen bg-[#021b30]">
           <div className="w-[1100px] h-[850px] overflow-hidden">
             <Image
               src={landingImage2}

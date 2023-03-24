7503e5f9d623baffa59a83f16412d6ce0c4f32d5
한상현 <xhaktm3344@gmail.com>
2023-03-24 18:01:35 +0900

    [FE] feature/signup : 쿠키에 토큰값 및 이메일 저장 middleware

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,23 +1,32 @@
 import '@/styles/globals.css';
-import type {AppProps} from 'next/app';
+import type {AppContext, AppProps} from 'next/app';
 import {wrapper} from 'store';
 import MusicBar from '@/components/MusicBar';
 import {useEffect, useState} from 'react';
 import {motion, AnimatePresence} from 'framer-motion';
 import {useRouter} from 'next/router';
+import cookies from 'next-cookies';
+import {getCookie} from 'cookies-next';
+import {setToken} from '@/components/TokenManager';
+import App from 'next/app';
 
-function App({Component, pageProps}: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
   const router = useRouter();
+
   const [isLogin, setIsLogin] = useState<boolean>(false);
 
   // 로그인 상태 체크
   useEffect(() => {
-    if (localStorage.getItem('accessToken')) {
+    // 쿠키 가져오고
+    const token = getCookie('accessToken');
+    if (token) {
+      console.log(token, '토큰 있음');
       setIsLogin(true);
     } else {
+      console.log(token, '토큰 없음');
       setIsLogin(false);
     }
-  }, []);
+  }, [router]);
   return (
     <>
       {isLogin && <MusicBar />}
@@ -35,5 +44,30 @@ function App({Component, pageProps}: AppProps) {
     </>
   );
 }
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps: any = await App.getInitialProps(appContext);
 
-export default wrapper.withRedux(App);
+  const {ctx} = appContext;
+  const allCookies = cookies(ctx);
+  const accessTokenByCookie = allCookies['accessToken'];
+
+  return {...appProps};
+};
+
+// App.getInitialProps = async (appContext: AppContext) => {
+//   const appProps: any = await App.getInitialProps(appContext)
+
+//   const {ctx} = appContext;
+//   const allCookies = cookies(ctx);
+//   console.log(allCookies, "쿠키데이터");
+//   const accessTokenByCookie = allCookies['accessToken'];
+
+//   if(accessTokenByCookie !== undefined) {
+//       const refreshTokenByCookie = (allCookies["refreshToken"] || "");
+//       setToken(accessTokenByCookie, refreshTokenByCookie)
+//   }
+
+//   return {...appProps}
+// }
+
+export default wrapper.withRedux(MyApp);

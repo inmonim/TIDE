58852420e6d3796b1d8653cd6aaf2213d6cf2b1d
rudtn0820 <rudtn0820@naver.com>
2023-03-26 21:31:57 +0900

    [BE] follow신청,목록 추가

---
+++ backend/user/src/main/java/com/muchu/user/request/UserNicknameRequest.java
@@ -0,0 +1,10 @@
+package com.muchu.user.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserNicknameRequest {
+    private String nickname;
+}

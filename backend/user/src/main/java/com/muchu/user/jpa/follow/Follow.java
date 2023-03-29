f843c1435a35cc27ebf9bdf1b45f5e0e7fe49c50
jsieon97 <rudtn0820@naver.com>
2023-03-29 12:22:40 +0900

    [BE] 팔로우 수정

--- backend/user/src/main/java/com/muchu/user/jpa/follow/Follow.java
+++ backend/user/src/main/java/com/muchu/user/jpa/follow/Follow.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Entity
 public class Follow {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "to_user")

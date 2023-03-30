21fca78aa44b3a9c1fefad988950c1de9a503d6c
jsieon97 <rudtn0820@naver.com>
2023-03-30 11:26:13 +0900

    [BE] 배포 Cors

--- backend/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ backend/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -19,7 +19,7 @@ import reactor.core.publisher.Mono;
 public class FilterConfig {
     private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, email";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
-    private static final String ALLOWED_ORIGIN = "http://localhost:3000";
+    private static final String ALLOWED_ORIGIN = "*";
     private static final String ALLOWED_CREDENTIALS = "true";
     private static final String MAX_AGE = "3600";
 

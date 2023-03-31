2bd3ae7d41ccf692a580150e79c0cecf4a9acaab
jsieon97 <rudtn0820@naver.com>
2023-03-31 17:18:28 +0900

    [BE] music파이프라인테스트

--- backend/music/src/main/java/com/tide/music/controller/music/MusicController.java
+++ backend/music/src/main/java/com/tide/music/controller/music/MusicController.java
@@ -1,4 +1,9 @@
 package com.tide.music.controller.music;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class MusicController {
 }

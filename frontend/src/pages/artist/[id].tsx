36125bde3d698ab335cfa235dca4905c088546f2
EHCha <chaeh0926@gmail.com>
2023-03-31 15:42:08 +0900

    [FE]/feature/music : music 페이지와 musicslice 업데이트

--- frontend/src/pages/artist/[id].tsx
+++ frontend/src/pages/artist/[id].tsx
@@ -29,7 +29,7 @@ function Artist() {
         className={`
       p-[4rem] pt-[2rem] lg12:pr-[calc(200px)] lg12:pl-[calc(15%+100px)] pb-[240px] text-[#eeeeee] flex flex-col min-h-[100vh] pt-[calc(2rem+40px)] bg-gradient-to-t from-blue-900 to-slate-900 `}>
         {/* <h1 className="text-center text-white">{router.query.id}</h1> */}
-        <p className="text-center text-white">{ArtistId}===</p>
+        <p className="text-center text-white">{ArtistId}</p>
       </main>
     </div>
   );

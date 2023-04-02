c8c107afa158cc8a92e407e099b562c038248032
rudtn0820 <rudtn0820@naver.com>
2023-04-02 21:17:33 +0900

    [BE] 플레이리스트상위6 아티스트상위6

--- backend/music/src/main/java/com/tide/music/jpa/userplaylist/UserPlaylistRepository.java
+++ backend/music/src/main/java/com/tide/music/jpa/userplaylist/UserPlaylistRepository.java
@@ -3,6 +3,7 @@ package com.tide.music.jpa.userplaylist;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,6 +17,6 @@ public interface UserPlaylistRepository extends JpaRepository<UserPlaylist, Long
 
     List<UserPlaylist> findAllByUserIdAndIsPublicNot(Long userId, String s);
 
-    @Query("SELECT p FROM UserPlaylist p ORDER BY p.likeCnt DESC")
-    List<UserPlaylist> findTopPlaylist(Pageable pageable);
+    @Query("SELECT p FROM UserPlaylist p WHERE p.isPublic = :isPublic ORDER BY p.likeCnt DESC")
+    List<UserPlaylist> findTopPlaylist(Pageable pageable, @Param("isPublic") String is);
 }

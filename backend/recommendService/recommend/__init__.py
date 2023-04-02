8e6b3b121aac5787631e6f4d20a239c1dd87971a
정인모 <cat10830@naver.com>
2023-04-03 03:19:17 +0900

    [BE]feature/recommend : 추천 서비스 완성 / song_album 데이터 이상 발견

--- backend/recommendService/recommend/__init__.py
+++ backend/recommendService/recommend/__init__.py
@@ -2,17 +2,11 @@ from flask import Flask, Blueprint, Response, json, g
 from flask_sqlalchemy import SQLAlchemy
 
 import pandas as pd
-
+import torch
 
 import config
 
-
 db = SQLAlchemy()
-
-
-from . import modules
-module = modules()
-
 
 def create_app():
     app = Flask(__name__)
@@ -21,21 +15,9 @@ def create_app():
     #ORM 활성화
     db.init_app(app)
     
-    from . import models
     
     from .views import song_recommend
-    
     app.register_blueprint(song_recommend.bp)
     
-    
-    @app.before_first_request
-    def load_data():
-        conn = db.engine.connect()
-        g.data = pd.read_sql_table('song_emotion_keyword', conn)
-    
-    
-    @app.route('/')
-    def test():
-        return ''
     
     return app

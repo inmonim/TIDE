187d3e7e58b7842fbaf731a122a8cac10c9e579c
정인모 <cat10830@naver.com>
2023-04-04 14:00:36 +0900

    (BE)faeture/recommend : 알고리즘 수정

--- backend/recommendService/recommend/__init__.py
+++ backend/recommendService/recommend/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 import pandas as pd
 import torch
@@ -10,6 +11,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*" : {"origins" : '*'}})
+    
     app.config.from_object(config)
     
     #ORM 활성화

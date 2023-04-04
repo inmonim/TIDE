966ab0993633693bf13d69ae21ebefa2de94f79f
정인모 <cat10830@naver.com>
2023-04-04 13:30:34 +0900

    (BE)feature/recommend : header 수정
    Former-commit-id: e4e1413c39152bc269a797bab3eb3ea1a2a73e87

--- backend/recommendService/recommend/views/song_recommend.py
+++ backend/recommendService/recommend/views/song_recommend.py
@@ -23,67 +23,70 @@ class MusicRecommend(Resource):
         
         txt = request.json['content']
         
-        # try:
-        response = {}
-        
-        emotion_list_1 = emotion_predict(model_1, txt)
-        emotion_list_2 = emotion_predict(model_2, txt)
-        
-        t3_model_1_song_id = recommend_song(t3_emotion_keysen, emotion_list_1)
-        t3_model_2_song_id = recommend_song(t3_emotion_keysen, emotion_list_2)
-        
-        song_id_list = recommend_cosine(ten_emotion, emotion_list_1) + recommend_cosine(ten_emotion, emotion_list_2) + [t3_model_1_song_id, t3_model_2_song_id]
-        recommend_list = {}
-        
-        song_cls = ['model1CosineTop', 'model2CosineTop', 'model1T3Top', 'model2T3Top']
-        cnt = 0
-        for i in song_id_list:
+        try:
+            response = {}
             
-            song_dict = {}
+            emotion_list_1 = emotion_predict(model_1, txt)
+            emotion_list_2 = emotion_predict(model_2, txt)
             
-            cls_nm = song_cls[cnt]
+            t3_model_1_song_id = recommend_song(t3_emotion_keysen, emotion_list_1)
+            t3_model_2_song_id = recommend_song(t3_emotion_keysen, emotion_list_2)
         
-            song = Song.query.filter(Song.song_id==i).first()
-            
-            song_dict['songId'] = song.song_id
-            song_dict['title'] = song.title
-            
-            album = SongAlbum.query.filter(SongAlbum.song_id==i).first()
-            
-            try:
-                album = album.album
-                song_dict['albumTitle'] = album.album_title
-                song_dict['albumImgPath'] = album.album_img_path
-            except:
-                song_dict['albumTitle'] = 'unknown album'
-                song_dict['albumImgPath'] = 'unknown_img'
                 
+            # 중요!
+            # 아래 song_id_list와 song_cls는 매핑되는 관계이므로, 순서를 잘 맞춰줘야 함! 
+            song_id_list = recommend_cosine(ten_emotion, emotion_list_2) + recommend_cosine(ten_emotion, emotion_list_1) + [t3_model_2_song_id, t3_model_1_song_id]
+            song_cls = ['model1CosineTop', 'model2CosineTop', 'model1T3Top', 'model2T3Top']
+            recommend_list = {}
+            
+            cnt = 0
+            for i in song_id_list:
                 
-            artists = SongArtist.query.filter(SongArtist.song_id==i).all()
-            artist_list = []
-            for ar in artists:
+                song_dict = {}
+                
+                cls_nm = song_cls[cnt]
+            
+                song = Song.query.filter(Song.song_id==i).first()
+                
+                song_dict['songId'] = song.song_id
+                song_dict['title'] = song.title
+                
+                album = SongAlbum.query.filter(SongAlbum.song_id==i).first()
+                
                 try:
-                    ar = ar.artist
-                    artist_nm = ar.artist_name
+                    album = album.album
+                    song_dict['albumTitle'] = album.album_title
+                    song_dict['albumImgPath'] = album.album_img_path
                 except:
-                    artist_nm = 'unknown artist'
+                    song_dict['albumTitle'] = 'unknown album'
+                    song_dict['albumImgPath'] = 'unknown_img'
                     
-                artist_list.append(artist_nm)
-            song_dict['artist'] = artist_list
-                
-            recommend_list[f'song_{cls_nm}'] = song_dict
-            cnt += 1
-        
-        response = recommend_list
-        
-        response = Response(json.dumps(response, ensure_ascii=False), content_type='application/json; charset=utf-8', status=200)
-        return response
-        # except AttributeError:
-        #     return Response(json.dumps({'error' : '노래 데이터 불러오기를 실패했습니다.'}, ensure_ascii=False), content_type='application/json; charset=utf-8', status=404)
-        # except NameError:
-        #     return Response(json.dumps({'error' : '모듈 불러오기에 실패했습니다. 관리자에게 문의 바랍니다.'}, ensure_ascii=False), content_type='application/json; charset=utf-8', status=500)
-        # # except:
-        #     return Response(json.dumps({'error' : '알 수 없는 오류가 발생했습니다. 관리자에게 문의 바랍니다.'}, ensure_ascii=False), content_type='application/json; charset=utf-8', status=500)
+                    
+                artists = SongArtist.query.filter(SongArtist.song_id==i).all()
+                artist_list = []
+                for ar in artists:
+                    try:
+                        ar = ar.artist
+                        artist_nm = ar.artist_name
+                    except:
+                        artist_nm = 'unknown artist'
+                        
+                    artist_list.append(artist_nm)
+                song_dict['artist'] = artist_list
+                    
+                recommend_list[f'song_{cls_nm}'] = song_dict
+                cnt += 1
+            
+            response = {'song_list' : recommend_list, }
+            
+            response = Response(json.dumps(response, ensure_ascii=False), headers=({'Access-Control-Allow-Origin': '*'}), content_type='application/json; charset=utf-8', status=200)
+            return response
+        except AttributeError:
+            return Response(json.dumps({'error' : '노래 데이터 불러오기를 실패했습니다.'}, ensure_ascii=False), content_type='application/json; charset=utf-8', status=404)
+        except NameError:
+            return Response(json.dumps({'error' : '모듈 불러오기에 실패했습니다. 관리자에게 문의 바랍니다.'}, ensure_ascii=False), content_type='application/json; charset=utf-8', status=500)
+        except:
+            return Response(json.dumps({'error' : '알 수 없는 오류가 발생했습니다. 관리자에게 문의 바랍니다.'}, ensure_ascii=False), content_type='application/json; charset=utf-8', status=500)
 
 
 class RefreshDf(Resource):
@@ -91,7 +94,7 @@ class RefreshDf(Resource):
         global ten_emotion, t3_emotion_keysen
         ten_emotion, t3_emotion_keysen = refresh_df()
         print(ten_emotion.tail())
-        return {'ok':200}
+        return {'status': 200, 'message': '음악 감정 분석 테이블 수정이 완료되었습니다.'}
 
 
 api.add_resource(MusicRecommend, '/diarymusic')

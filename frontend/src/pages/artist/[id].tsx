52bca667976830aef693b772afc7e5dbdab865f8
EHCha <chaeh0926@gmail.com>
2023-04-05 15:13:17 +0900

    [FE]/feature/landing : 로그인 가는거 바꿈

--- frontend/src/pages/artist/[id].tsx
+++ frontend/src/pages/artist/[id].tsx
@@ -1,20 +1,47 @@
 import Head from 'next/head';
 import {useRouter} from 'next/router';
+import {useEffect} from 'react';
+import {useAppSelector, useAppDispatch} from 'store';
+import {artistAsync} from 'store/api/features/artistSlice';
 
 function Artist() {
   const router = useRouter();
 
-  const ArtistId = router.query.id;
+  const {id: ArtistId} = router.query;
+
+  const dispatch = useAppDispatch();
+
+  const {artistId, artistName, artistImgPath, is_group} = useAppSelector(
+    state => state.artist
+  );
+
+  console.log(artistName, artistImgPath);
+
+  useEffect(() => {
+    if (typeof ArtistId === 'string') {
+      dispatch(artistAsync(ArtistId));
+    }
+  }, [ArtistId]);
 
   return (
     <div>
       <Head>
-        <title>{router.query.name}</title>
+        <title>{artistName}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
-        {/* <h1 className="text-center text-white">{router.query.id}</h1> */}
-        <p className="text-center text-white">{ArtistId}</p>
+      <main className="flex flex-col min-h-[91vh]">
+        <div
+          className="w-[88%] h-[500px] ml-[12%] bg-cover bg-no-repeat"
+          style={{
+            backgroundImage: `url(${artistImgPath})`,
+            backgroundPosition: 'center',
+            opacity: 0.7
+          }}></div>
+        <div className="w-[88%] h-[400px] ml-[12%] bg-gradient-to-t from-slate-700 to-slate-900">
+          <div className="flex flex-row justify-center text-5xl text-center text-white ">
+            {artistName}
+          </div>
+        </div>
       </main>
     </div>
   );

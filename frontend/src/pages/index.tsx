c0181e4eee4ef88e12826005eafaf8eed8954da1
EHCha <chaeh0926@gmail.com>
2023-04-05 17:12:08 +0900

    [FE]/feature/artist : 아티스트 페이지 완성 아티스트의 노래로 가기 가능

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import {Inter} from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import Landing from './landing';
+// import Landing from './landing';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className={styles.main}>
-        <Landing />
+        {/* <Landing /> */}
         <div className={styles.description}></div>
       </main>
     </>
